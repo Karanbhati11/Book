a4c92d56f4c796eaa05e72f6fdbb8134a42746df
Karan Bhati <67897812+Karanbhati11@users.noreply.github.com>
2022-09-12 08:53:58 +0530

    Update Retrieve.js

--- src/functions/Retrieve.js
+++ src/functions/Retrieve.js
@@ -3,7 +3,7 @@ async function searchFile() {
   const { google } = require("googleapis");
   const path = require("path");
   const auth = new google.auth.GoogleAuth({
-    keyFile: "var/task/functions/keys.json",
+    keyFile: "functions/keys.json",
     scopes: ["https://www.googleapis.com/auth/drive"],
   });
   const service = google.drive({ version: "v3", auth });
